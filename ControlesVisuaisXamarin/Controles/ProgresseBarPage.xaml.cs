ab69e6544c31e3b1d16c3a0bcf134a68f72e09b3
BRENO MORAIS <62041808+BREN0-MORAIS@users.noreply.github.com>
2021-01-07 21:53:36 -0400

    Initial commit

---
+++ ControlesVisuaisXamarin/Controles/ProgresseBarPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ControlesVisuaisXamarin.Controles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProgresseBarPage : ContentPage
+    {
+        public ProgresseBarPage()
+        {
+            InitializeComponent();
+        
+        }
+
+        private void Modificar(object sender, EventArgs e)
+        {
+            Bar1.Progress =+ 1;
+            Bar2.ProgressTo(1, 5000,Easing.Linear);
+            Bar3.ProgressTo(1, 6000, Easing.SpringIn);
+        }
+    }
+}
---
+++ ControlesVisuaisXamarin/Menu/MaterPag.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ControlesVisuaisXamarin.Menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MaterPag : MasterDetailPage
+    {
+        public MaterPag()
+        {
+            InitializeComponent();
+        }
+
+
+        private void GoActivit(object sender, EventArgs args)
+        {
+            Detail = new Controles.ActivityIndicatorPage();
+        }  
+        private void GoProgressBarPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.ProgresseBarPage();
+        } 
+        private void GoBoxViewPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.BoxPage();
+        }  
+        private void GoLabel(object sender, EventArgs args)
+        {
+            Detail = new Controles.Label();
+        }  
+        private void GoButton(object sender, EventArgs args)
+        {
+            Detail = new Controles.ButtonPage();
+        }  
+        private void GoEntryEditorPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.EntryEditorPage();
+        }
+        private void GoDataPickerPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.DatePickerPage();
+        }
+        private void GoTimerPickerPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.TimerPickerPage();
+        }
+  
+        private void GoPickerPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.PickerPage();
+        }
+        private void GoSerchBarPage(object sender, EventArgs args)
+        {
+            Detail = new Controles.SerchBarPage();
+        }
+    }
+}
