79a10ad98a9c016469663859c21fbbe0481cd2e5
BRENO MORAIS <62041808+BREN0-MORAIS@users.noreply.github.com>
2021-01-23 20:39:36 -0400

    Solução
    Solução Adicionada.

---
+++ ControlesVisuaisXamarin/ControlesVisuaisXamarin/Controles/DatePickerPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ControlesVisuaisXamarin.Controles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DatePickerPage : ContentPage
+    {
+        public DatePickerPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Action(object sender, DateChangedEventArgs args)
+        {
+            Resultado.Text = args.OldDate.ToString("dd/mm/yyyy") +  "" + "" + args.NewDate.ToString("dd/mm/yyyy");
+        }
+    }
+}
---
+++ ControlesVisuaisXamarin/ControlesVisuaisXamarin/Controles/EntryEditorPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ControlesVisuaisXamarin.Controles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntryEditorPage : ContentPage
+    {
+        public EntryEditorPage()
+        {
+            InitializeComponent();
+            //faz com que aparça em tempo real
+            TxtIdade.TextChanged += delegate (object sender,TextChangedEventArgs args) {
+
+                Lbl_Duplicado.Text = args.OldTextValue;
+            };
+
+            //conta a quantifdade de caractere quando tira o foco do Edito
+            TxtComentario.Completed += delegate(Object sender, EventArgs args){
+              LblQuantidadeCaracteres.Text =   TxtComentario.Text.Length.ToString();
+            };
+        }
+
+       
+    }
+}
